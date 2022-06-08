f50c662d0db9c0c25f35f2485df48c2e5ad8b6d9
shuai Wang <shuaiwang@bjyz-dl-001.autox.sz>
2022-06-08 19:12:54 +0800

    complete lesson-8-article

--- api/v1/article.go
+++ api/v1/article.go
@@ -1,11 +1,77 @@
 package v1
 
-// add article
+import (
+	"ginblog/model"
+	"ginblog/utils/errmsg"
+	"net/http"
+	"strconv"
 
-// search single article
+	"github.com/gin-gonic/gin"
+)
 
-// search article list
+// CreateArticle
+func AddArt(c *gin.Context) {
+	var data model.Article
+	_ = c.ShouldBindJSON(&data)
 
-// delete article
+	code = model.CreateArt(&data)
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"data":    data,
+		"message": errmsg.GetErrMsg(code),
+	})
+}
 
-// upate article
+// todo search all articles with a category
+
+// todo search one article
+
+// todo GetArticle list
+func GetArt(c *gin.Context) {
+	pageSize, _ := strconv.Atoi(c.Query("pagesize"))
+	pageNum, _ := strconv.Atoi(c.Query("pagenum"))
+	title := c.Query("title")
+
+	switch {
+	case pageSize >= 100:
+		pageSize = 100
+	case pageSize < 0:
+		pageSize = 10
+	}
+	if pageNum == 0 {
+		pageNum = 1
+	}
+
+	code = errmsg.SUCCESS
+	data, total := model.GetArt(title, pageSize, pageNum)
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"data":    data,
+		"total":   total,
+		"message": errmsg.GetErrMsg(code),
+	})
+}
+
+// EditArticle
+func EditArt(c *gin.Context) {
+	var data model.Article
+	id, _ := strconv.Atoi(c.Param("id"))
+	c.ShouldBindJSON(&data)
+	code = model.EditArt(id, &data)
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"message": errmsg.GetErrMsg(code),
+	})
+}
+
+// DeleteArticle
+func DeleteArt(c *gin.Context) {
+	cid, _ := strconv.Atoi(c.Param("cid"))
+
+	code := model.DeleteArt(cid)
+
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"message": errmsg.GetErrMsg(code),
+	})
+}
--- model/Article.go
+++ model/Article.go
@@ -1,6 +1,10 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"ginblog/utils/errmsg"
+
+	"gorm.io/gorm"
+)
 
 type Article struct {
 	Category Category `gorm:"foreignkey:Cid"`
@@ -10,4 +14,57 @@ type Article struct {
 	Desc    string `gorm:"type:varchar(1024)" json:"desc"`
 	Content string `gorm:"type:text" json:"content"`
 	Img     string `gorm:"type:varchar(128)" json:"img"`
+}
+
+// CreateArticle
+func CreateArt(data *Article) int {
+	err := db.Create(&data).Error
+	if err != nil {
+		return errmsg.ERROR
+	}
+	return errmsg.SUCCESS
+}
+
+// todo search all articles with one category
+
+// todo search one article
+
+// todo GetArticle list
+func GetArt(title string, pageSize int, pageNum int) ([]Article, int64) {
+	var art []Article
+	var total int64
+
+	err := db.Find(&art).Limit(pageSize).Offset((pageNum - 1) * pageNum).Error
+	db.Model(&art).Count(&total)
+	if err != nil {
+		return nil, 0
+	}
+	return art, total
+}
+
+// EditArticle
+func EditArt(id int, data *Article) int {
+	var art Article
+	var maps = make(map[string]interface{})
+	maps["title"] = data.Title
+	maps["cid"] = data.Cid
+	maps["desc"] = data.Desc
+	maps["content"] = data.Content
+	maps["img"] = data.Img
+
+	err := db.Model(&art).Where("id = ?", id).Updates(&maps).Error
+	if err != nil {
+		return errmsg.ERROR
+	}
+	return errmsg.SUCCESS
+}
+
+// DeleteArticle
+func DeleteArt(id int) int {
+	var art Article
+	err := db.Where("id = ?", id).Delete(&art).Error
+	if err != nil {
+		return errmsg.ERROR
+	}
+	return errmsg.SUCCESS
 }
