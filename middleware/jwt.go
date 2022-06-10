bab744e000b6e2923e41cd065f96be73fbdf5b60
shuai Wang <shuaiwang@bjyz-dl-001.autox.sz>
2022-06-10 14:53:30 +0800

    complete lesson-11-login

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -15,24 +15,22 @@ var JwtKey = []byte(utils.JwtKey)
 
 type MyClaims struct {
 	Username string `json:"username"`
-	Password string `json:"password"`
 	jwt.RegisteredClaims
 }
 
 // generate token
-func GenToken(username string, password string) (string, int) {
+func GenToken(username string) (string, int) {
 	expireTime := time.Now().Add(10 * time.Hour)
 	expireAt := jwt.NewNumericDate(expireTime)
 	SetClaims := MyClaims{
 		Username: username,
-		Password: password,
 		RegisteredClaims: jwt.RegisteredClaims{
 			ExpiresAt: expireAt,
 			Issuer:    "ginblog",
 		},
 	}
 
-	reqClaim := jwt.NewWithClaims(jwt.SigningMethodES256, SetClaims)
+	reqClaim := jwt.NewWithClaims(jwt.SigningMethodHS256, SetClaims)
 	token, err := reqClaim.SignedString(JwtKey)
 	if err != nil {
 		return "", errmsg.ERROR
@@ -43,10 +41,10 @@ func GenToken(username string, password string) (string, int) {
 
 // verify token
 func CheckToken(token string) (*MyClaims, int) {
-	settoken, _ := jwt.ParseWithClaims(token, &MyClaims{}, func(token *jwt.Token) (interface{}, error) {
+	setToken, _ := jwt.ParseWithClaims(token, &MyClaims{}, func(token *jwt.Token) (interface{}, error) {
 		return JwtKey, nil
 	})
-	if key, ok := settoken.Claims.(*MyClaims); ok && settoken.Valid {
+	if key, _ := setToken.Claims.(*MyClaims); setToken.Valid {
 		return key, errmsg.SUCCESS
 	} else {
 		return nil, errmsg.ERROR
@@ -60,25 +58,43 @@ func JwtToken() gin.HandlerFunc {
 		tokenHeader := c.Request.Header.Get("Authorization")
 		if tokenHeader == "" {
 			code = errmsg.ERROR_TOKEN_NOT_EXIST
+			c.JSON(http.StatusOK, gin.H{
+				"code":    code,
+				"message": errmsg.GetErrMsg(code),
+			})
+			c.Abort()
+			return
 		}
 		checkToken := strings.SplitN(tokenHeader, " ", 2)
 		if len(checkToken) != 2 && checkToken[0] != "Bearer" {
 			code = errmsg.ERROR_TOKEN_WRONG_FORMAT
+			c.JSON(http.StatusOK, gin.H{
+				"code":    code,
+				"message": errmsg.GetErrMsg(code),
+			})
 			c.Abort()
+			return
 		}
-		key, Tcode := CheckToken(checkToken[1])
-		if Tcode == errmsg.ERROR {
+		key, tCode := CheckToken(checkToken[1])
+		if tCode == errmsg.ERROR {
 			code = errmsg.ERROR_TOKEN_WRONG
+			c.JSON(http.StatusOK, gin.H{
+				"code":    code,
+				"message": errmsg.GetErrMsg(code),
+			})
 			c.Abort()
+			return
 		}
 		if time.Now().Unix() > key.ExpiresAt.Unix() {
 			code = errmsg.ERROR_TOKEN_EXPIRED
+			c.JSON(http.StatusOK, gin.H{
+				"code":    code,
+				"message": errmsg.GetErrMsg(code),
+			})
 			c.Abort()
+			return
 		}
-		c.JSON(http.StatusOK, gin.H{
-			"code":    code,
-			"message": errmsg.GetErrMsg(code),
-		})
+
 		c.Set("Username", key.Username)
 		c.Next()
 	}
